ca523f9847e8f06aadb06bb59ec177e8c3a60895
Atralupus <me@atralupus.com>
2022-12-24 02:15:14 +0900

    state

--- autochess-simulation/Assets/Scripts/Actions/InitSessionPlainValue.cs
+++ autochess-simulation/Assets/Scripts/Actions/InitSessionPlainValue.cs
@@ -1,20 +1,19 @@
-using Libplanet.Store;
 using System.Collections.Immutable;
+using Libplanet.Store;
 
 namespace Scripts.Actions
 {
-    public class ClickActionPlainValue : DataModel
+    public class InitSessionActionPlainValue : DataModel
     {
         public ImmutableList<int> Count { get; private set; }
 
-        public ClickActionPlainValue(ImmutableList<int> count)
+        public InitSessionActionPlainValue(ImmutableList<int> count)
             : base()
         {
             Count = count;
         }
 
-        // Used for deserializing stored action.
-        public ClickActionPlainValue(Bencodex.Types.Dictionary encoded)
+        public InitSessionActionPlainValue(Bencodex.Types.Dictionary encoded)
             : base(encoded)
         {
         }
